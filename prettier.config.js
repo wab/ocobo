48d88fafb50d0b95300e10e17c1e651a08742c45
jérôme boileux <wab2com@gmail.com>
2022-10-21 17:30:05 +0200

    🔧 init configurations & tailwind theme

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "ignore",
+  arrowParens: "always",
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      light: '#d0dbdd',
+      blue: '#98c0d9',
+      dark: '#293241',
+      mint: '#9adbba',
+      papaya: '#ffa266',
+      coral: '#ff6f61',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
