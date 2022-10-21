0048474c0c65efa017d2d30de001f36bf1218765
jérôme boileux <wab2com@gmail.com>
2022-10-21 15:42:11 +0200

    🎉 init ocobo website with remix

---
+++ app/routes/index.tsx
@@ -0,0 +1,12 @@
+export default function Index() {
+  return (
+    <div
+      style={{
+        fontFamily: 'system-ui, sans-serif',
+        lineHeight: '1.4',
+      }}
+    >
+      <h1>🏗️ Welcome to Ocobo website [wip]</h1>
+    </div>
+  );
+}
