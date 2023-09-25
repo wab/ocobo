103c188d67bcbdd2a34d95a4e5541442f5ca70df
jérôme boileux <wab2com@gmail.com>
2023-09-25 22:05:57 +0200

    🚩 opening remix V2 flags

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,4 @@
+const { createRoutesFromFolders } = require('@remix-run/v1-route-convention');
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   serverBuildTarget: 'vercel',
@@ -10,4 +11,14 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
+  future: {
+    v2_dev: true,
+    v2_errorBoundary: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes);
+  },
 };
