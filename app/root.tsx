da2fdd5e21cdeecb22532c8184f54ce9587b1475
jérôme boileux <wab2com@gmail.com>
2024-04-25 18:05:48 +0200

    💄 update favicon

--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,27 @@ import { useChangeLanguage } from 'remix-i18next/react';
 import styles from '~/index.css?url';
 import { getLang } from '~/utils/lang';
 
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: styles },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: 'apple-touch-icon.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicon-32x32.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicon-16x16.png',
+  },
+  { rel: 'manifest', href: '/site.webmanifest' },
+];
 
 export const handle = { i18n: ['common'] };
 
@@ -36,6 +56,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* TODO: remove this when in production */}
         <meta name="robots" content="noindex, nofollow" />
+
         <Meta />
         <Links />
       </head>
