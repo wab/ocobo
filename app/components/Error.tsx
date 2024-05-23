5e0a24dea8f23081273d02e995527d384fdb9f35
jérôme boileux <wab2com@gmail.com>
2024-05-23 10:02:17 +0200

    💄 add error boundary and suspense in main layout

---
+++ app/components/Error.tsx
@@ -0,0 +1,45 @@
+import { NavLink } from '@remix-run/react';
+import { BombIcon } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
+
+import { css } from '@ocobo/styled-system/css';
+import { Container } from '@ocobo/styled-system/jsx';
+import { button, icon } from '@ocobo/styled-system/recipes';
+
+import { url } from '~/utils/url';
+
+const Message: React.FunctionComponent<
+  React.PropsWithChildren<unknown>
+> = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div
+      className={css({
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 'calc(100vh - 452px)',
+      })}
+    >
+      <BombIcon className={icon({ size: 'xl' })} />
+      <h1
+        className={css({
+          textStyle: 'heading2',
+          mt: 4,
+        })}
+      >
+        {t('error.title')}
+      </h1>
+      <p>{t('error.description')}</p>
+      <p>
+        <NavLink to={url.homepage} className={button({ variant: 'solid' })}>
+          {t('error.back')}
+        </NavLink>
+      </p>
+    </div>
+  );
+};
+
+export const Error = { Container, Message };
