c2b5f9e656b19dfa1727a33f097b92aa8f990141
jérôme boileux <wab2com@gmail.com>
2024-05-23 22:24:22 +0200

    🔧 add robots and sitemap files

---
+++ app/routes/sitemap[.xml].tsx
@@ -0,0 +1,68 @@
+import { fetchBlogPosts, fetchStories } from '~/modules/utils.server';
+
+const baseUrl = 'https://www.ocobo.co';
+
+function getUrlElementWithDate(url: string, date: string) {
+  return `<url>
+        <loc>${url}</loc>
+        <lastmod>${date}</lastmod>
+        </url>`;
+}
+
+function generateSiteMap(
+  stories: { slug: string; date: Date }[],
+  posts: { slug: string; date: Date }[],
+) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+            ${getUrlElementWithDate(`${baseUrl}/fr`, new Date().toISOString())}
+            ${getUrlElementWithDate(`${baseUrl}/fr/strategies-revenue-operations`, new Date().toISOString())}
+            ${getUrlElementWithDate(`${baseUrl}/fr/projets-revops`, new Date().toISOString())}
+            ${getUrlElementWithDate(`${baseUrl}/fr/contact`, new Date().toISOString())}
+            ${getUrlElementWithDate(`${baseUrl}/stories`, new Date().toISOString())}
+
+            ${stories
+              .map(
+                (story) =>
+                  `${getUrlElementWithDate(
+                    `${baseUrl}/stories/${story.slug}`,
+                    story.date.toISOString(),
+                  )}`,
+              )
+              .join('\n')}
+            ${getUrlElementWithDate(`${baseUrl}/blog`, new Date().toISOString())}
+            ${posts
+              .map(
+                (post) =>
+                  `${getUrlElementWithDate(
+                    `${baseUrl}/blog/${encodeURIComponent(post.slug)}`,
+                    post.date.toISOString(),
+                  )}`,
+              )
+              .join('\n')}
+            ${getUrlElementWithDate(`${baseUrl}/legal/confidentialite`, new Date().toISOString())}
+            ${getUrlElementWithDate(`${baseUrl}/legal/cgu`, new Date().toISOString())}
+        </urlset>`;
+}
+
+export async function loader() {
+  const storiesQuery = fetchStories().then((stories) =>
+    stories.map((story) => ({
+      slug: story.slug,
+      date: new Date(story.frontmatter.date),
+    })),
+  );
+  const postsQuery = fetchBlogPosts().then((stories) =>
+    stories.map((story) => ({
+      slug: story.slug,
+      date: new Date(story.frontmatter.date),
+    })),
+  );
+
+  const [stories, posts] = await Promise.all([storiesQuery, postsQuery]);
+  return new Response(generateSiteMap(stories, posts), {
+    headers: {
+      'content-type': 'application/xml',
+    },
+  });
+}
